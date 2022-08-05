c07517b3d87fda11c61d14afab614b17368517ca
Kazeazen <jamesthomason6152@gmail.com>
2022-08-05 13:36:26 -0700

    css, settings adjustments

--- frontend2/src/components/settings.js
+++ frontend2/src/components/settings.js
@@ -1,24 +1,22 @@
 import React, {useContext} from 'react';
 import AuthContext from '../context/AuthContext';
 import styles from "./cssModules/Homepage.module.css";
-import {Container, Label, Button} from 'reactstrap';
+import {Container, Button} from 'reactstrap';
 import jwtDecode from 'jwt-decode';
 const Settings = () => {
-    let {user, authToken} = useContext(AuthContext)
-    let {email} = jwtDecode(localStorage.getItem("authToken"))["sub"]
+    let {user} = useContext(AuthContext);
+    let {email} = jwtDecode(localStorage.getItem("authToken"))["sub"];
     
     return (
-        <div className={styles.profileContainer}>
-            <h1> {user}'s profile</h1>
-            <Container className={styles.innerProfileContainer}>                
-                <h2>Username:</h2><span>{user}</span>
-                <br></br>
-                <h2>Email: </h2> <span>{email}</span>
-                <br></br>
-                <br></br>
-                <Button type="button">Reset Password</Button>
-                
-            </Container>
+        <div>
+            <h1 className={styles.profileHeader}> {user}'s profile</h1>
+            <div className={styles.profileContainer}>
+                <div className={styles.innerProfileContainer}>                
+                    <h2>Username: {user}</h2> 
+                    <h2>Email: {email}</h2>
+                    <Button type="button">Reset Password</Button>
+                </div>
+            </div>
         </div>
        
     )
